65a22a7dc47aeca5a9366c4155f31a3622c57560
unknown <hirunahansindu123@gmail.com>
2026-04-09 22:29:19 +0530

    add senser service

---
+++ E-Commerce/senser-service/senser-service/src/test/java/com/example/senser_service/SenserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.senser_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
